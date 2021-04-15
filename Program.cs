e7b150975f62218607f5acf6eec2e9af5df71a99
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-15 13:38:53 -0300

    Obtendo a opção

--- Program.cs
+++ Program.cs
@@ -13,10 +13,14 @@ namespace StopWatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
         }
 
         static void Start(int time)
